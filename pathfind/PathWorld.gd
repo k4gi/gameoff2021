179f7f593ca4a17d7ce515df94bb619cbfc21c12
kagi <k4gi@pm.me>
2021-11-23 16:45:26 +1100

    i try to get my head around this nonsense. i think im getting there

--- pathfind/PathWorld.gd
+++ pathfind/PathWorld.gd
@@ -9,9 +9,20 @@ var test_grid
 func _ready():
 	test_grid = PATH_GRID.new( get_global_position(), Vector2(256, 128), 16 )
 	show_grid()
+	test_grid.grid[4][4].content = PATH_GRID.OBSTACLE
+	refresh_grid()
 
 
 func show_grid():
 	for y in test_grid.grid.size():
 		for x in test_grid.grid[y].size():
 			add_child(test_grid.tile_textures[y][x])
+
+
+func refresh_grid():
+	for y in test_grid.grid.size():
+		for x in test_grid.grid[y].size():
+			if test_grid.grid[y][x].content == PATH_GRID.OBSTACLE:
+				test_grid.tile_textures[y][x].set_modulate(Color("000000"))
+			elif test_grid.tile_textures[y][x].get_modulate() != Color("ffffff"):
+				test_grid.tile_textures[y][x].set_modulate(Color("ffffff"))
--- pathfind/path_grid.gd
+++ pathfind/path_grid.gd
@@ -3,16 +3,20 @@ const TILE_TEXTURE = preload("res://pathfind/Tile.tscn")
 
 enum {
 	EMPTY,
-	OBSTACLE
+	OBSTACLE,
+	OPEN,
+	CLOSED,
+	BEGINNING,
+	DESTINATION
 }
 
 var grid = []
 var tile_textures = []
+var resolution = 0
 
 
-func _init(top_left: Vector2, bottom_right: Vector2, resolution: int):
-	# i feel like i'm getting ahead of myself somehow
-	#but thats ok. gotta start somewhere.
+func _init(top_left: Vector2, bottom_right: Vector2, res: int):
+	resolution = res
 	var grid_size = bottom_right - top_left
 	
 	if int(grid_size.x) % resolution > 0 or int(grid_size.y) % resolution > 0:
@@ -20,7 +24,7 @@ func _init(top_left: Vector2, bottom_right: Vector2, resolution: int):
 		return
 	
 	build_grid( grid_size.x / resolution, grid_size.y / resolution )
-	build_textures( top_left, resolution, grid_size.x / resolution, grid_size.y / resolution )
+	build_textures( top_left, grid_size.x / resolution, grid_size.y / resolution )
 
 
 func build_grid(size_x: int, size_y: int):
@@ -30,9 +34,58 @@ func build_grid(size_x: int, size_y: int):
 			grid[y].append( PATH_NODE.new( EMPTY, x, y ) )
 
 
-func build_textures(top_left: Vector2, resolution: int, size_x: int, size_y: int):
+func build_textures(top_left: Vector2, size_x: int, size_y: int):
 	for y in range(size_y):
 		tile_textures.append( [] )
 		for x in range(size_x):
 			tile_textures[y].append( TILE_TEXTURE.instance() )
 			tile_textures[y][x].set_global_position(Vector2( top_left.x + x*resolution, top_left.y + y*resolution ) )
+
+
+func solve_path(beginning: Vector2, destination: Vector2):
+	var beginning_x = beginning.x / resolution
+	var beginning_y = beginning.y / resolution
+	var destination_x = destination.x / resolution
+	var destination_y = destination.y / resolution
+	
+	grid[beginning_y][beginning_x].content = BEGINNING
+	grid[destination_y][destination_x].content = DESTINATION
+	
+	var open_set = []
+	var closed_set = []
+	
+	open_set.append( grid[beginning_y][beginning_x] )
+	
+	for n in find_neighbours( open_set[0] ):
+		pass #to be continued
+
+
+func find_cost( node_a: PATH_NODE, node_b: PATH_NODE ):
+	var distance_x = abs( node_a.grid_x - node_b.grid_x )
+	var distance_y = abs( node_a.grid_y - node_b.grid_y )
+	
+	if distance_y < distance_x:
+		return distance_y * 14 + ( distance_x - distance_y ) * 10
+	else:
+		return distance_x * 14 + ( distance_y - distance_x ) * 10
+
+
+func find_neighbours( current_node: PATH_NODE ):
+	var neighbours = []
+	
+	var y = -1
+	while y < 2:
+		if current_node.grid_y + y < 0 or current_node.grid_y + y >= grid.size():
+			pass
+		else:
+			var x = -1
+			while x < 2:
+				if current_node.grid_x + x < 0 or current_node.grid_x + x >= grid[y].size():
+					pass
+				elif y == 0 and x == 0:
+					pass
+				else:
+					neighbours.append( grid[current_node.grid_y + y][current_node.grid_x + x] )
+				x += 1
+		y += 1
+	return neighbours
